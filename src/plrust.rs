cc2b91c5b3bb52ff7ee2abf6d6cf95b559d92021
Eric B. Ridge <eebbrr@gmail.com>
2020-11-29 12:36:28 -0700

    owned Vec's need to be of type Option<T>

--- src/plrust.rs
+++ src/plrust.rs
@@ -432,7 +432,7 @@ fn make_rust_type(type_oid: &PgOid, owned: bool) -> String {
     .to_string();
 
     if array && owned {
-        rust_type = format!("Vec<{}>", rust_type);
+        rust_type = format!("Vec<Option<{}>>", rust_type);
     } else if array {
         rust_type = format!("Array<{}>", rust_type);
     }
