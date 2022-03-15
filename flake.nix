67452a6ffdbaa0d673530962748da288a8b98915
Ana Hobden <operator@hoverbear.org>
2022-03-15 12:09:28 -0700

    Use develop pgx
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     naersk.inputs.nixpkgs.follows = "nixpkgs";
     gitignore.url = "git+https://github.com/hercules-ci/gitignore.nix";
     gitignore.inputs.nixpkgs.follows = "nixpkgs";
-    pgx.url = "git+https://github.com/zombodb/pgx?ref=nix-non-singlestep";
+    pgx.url = "git+https://github.com/zombodb/pgx?ref=develop";
     pgx.inputs.nixpkgs.follows = "nixpkgs";
     pgx.inputs.naersk.follows = "naersk";
   };
