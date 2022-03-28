6f83911ac71ea0170eb8a2df5425c6cf636f17e0
Ana Hobden <operator@hoverbear.org>
2022-03-28 15:49:42 -0700

    Include components dir
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

---
+++ components/plrust_interface/src/lib.rs
@@ -0,0 +1,26 @@
+
+#[cfg(feature = "host")]
+pub fn create_linker_functions(linker: &mut wasmtime::Linker<wasmtime_wasi::WasiCtx>) -> Result<(), wasmtime_wasi::Error> {
+    linker.func_wrap("plrust_interface", "spi_exec_select_num", crate::spi_exec_select_num)?;
+    Ok(())
+}
+
+
+#[cfg(feature = "guest")]
+#[link(wasm_import_module = "plrust_interface")]
+extern "C" {
+    pub fn spi_exec_select_num(i: i32) -> i32; // This simply does "SELECT i"
+}
+
+#[cfg(feature = "host")]
+pub fn spi_exec_select_num(i: i32) -> i32 {
+    match pgx::Spi::get_one(format!("SELECT {}", i).as_str()) {
+        Some(res) => {
+            return res;
+        }
+        None => {
+            pgx::warning!("Spi::get_one returned nothing, returning default value");
+            return -1;
+        }
+    }
+}
