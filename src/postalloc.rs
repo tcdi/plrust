4d3860eb10646042a5e66a7cc06fb51e12422cdb
Jubilee Young <j_young@tcdi.com>
2022-04-29 16:56:09 -0700

    Add counter

--- src/postalloc.rs
+++ src/postalloc.rs
@@ -1,5 +1,7 @@
 use core::alloc::{GlobalAlloc, Layout};
-use pgx::pg_sys::{self, SPI_palloc, SPI_repalloc, SPI_pfree};
+use core::sync::atomic::{AtomicUsize, Ordering};
+use std::io::Write;
+use pgx::pg_sys::{self, SPI_palloc, SPI_pfree};
 use pgx::log;
 
 struct PostAlloc;
@@ -7,14 +9,24 @@ struct PostAlloc;
 #[global_allocator]
 static PALLOC: PostAlloc = PostAlloc;
 
+static COUNTER: AtomicUsize = AtomicUsize::new(0);
+
 unsafe impl GlobalAlloc for PostAlloc {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let msg = "attempt to emit alloc message from custom allocator";
+        let mut buf = [0u8; 256];
+        let counter = COUNTER.fetch_add(1, Ordering::AcqRel);
+        let mut msg = &mut buf;
+        (&mut msg[..]).write_fmt(format_args!("allocated/deallocated at least {counter} times."));
+        msg[255] = 0; // Guarantee null termination for the C writer.
         pg_sys::write_stderr(msg as *const _ as *const i8);
         SPI_palloc(layout.size()).cast()
     }
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        let msg = "attempt to emit dealloc message from custom allocator";
+        let mut buf = [0u8; 256];
+        let counter = COUNTER.fetch_add(1, Ordering::AcqRel);
+        let mut msg = &mut buf;
+        (&mut msg[..]).write_fmt(format_args!("allocated/deallocated at least {counter} times."));
+        msg[255] = 0; // Guarantee null termination for the C writer.
         pg_sys::write_stderr(msg as *const _ as *const i8);
         SPI_pfree(ptr.cast())
     }
