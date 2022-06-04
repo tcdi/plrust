c7ce3a7e25b41165762724d104e63931db544a31
Jubilee Young <j_young@tcdi.com>
2022-06-04 00:29:06 -0700

    use latest version

--- src/plrust.rs
+++ src/plrust.rs
@@ -284,7 +284,7 @@ fn generate_cargo_toml(
         default = [ /* PG major version feature here */ ]
 
         [dependencies]
-        pgx = { git = "https://github.com/tcdi/pgx.git", rev = "7b62c9c2b45f772e2188fc3b2aefd8e120e4cc87", features = ["postgrestd"] }
+        pgx = { version = "0.5.0-beta.0", features = ["postgrestd"] }
         /* User deps here */
 
         [profile.release]
