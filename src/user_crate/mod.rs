b214fd04321f2a8f27007f0837a467d410c50341
John Hsu <hsuchen@amazon.com>
2022-11-22 19:30:22 +0000

    Get rid of old merge code

--- src/user_crate/mod.rs
+++ src/user_crate/mod.rs
@@ -230,10 +230,6 @@ fn parse_source_and_deps(code_and_deps: &str) -> eyre::Result<(syn::Block, toml:
 
     let user_dependencies = check_user_dependencies(deps_block)?;
 
-    if crate::gucs::allow_listed_dependencies_only() {
-        validate_dependences_are_allowed(&user_dependencies);
-    }
-
     let user_code: syn::Block =
         syn::parse_str(&code_block).map_err(PlRustError::ParsingCodeBlock)?;
 
