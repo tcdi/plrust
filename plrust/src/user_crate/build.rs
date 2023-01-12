d32385991cec5295e49cdc1a5dc4b2a53068e071
Eric B. Ridge <eebbrr@gmail.com>
2023-01-12 17:07:51 -0500

    bugfix

--- plrust/src/user_crate/build.rs
+++ plrust/src/user_crate/build.rs
@@ -108,7 +108,7 @@ impl FnBuild {
                     "CARGO_TARGET_{}_LINKER",
                     &target_triple.as_str().replace('-', "_").to_uppercase()
                 ),
-                &target_triple,
+                &format!("{}-gcc", target_triple),
             );
         }
 
