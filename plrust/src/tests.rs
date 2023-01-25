a58c9138543d8e19903ceb3ffba299236d959824
Jubilee <46493976+workingjubilee@users.noreply.github.com>
2023-01-25 08:41:10 -0800

    Test `#[export_name]` is blocked (#177)

--- plrust/src/tests.rs
+++ plrust/src/tests.rs
@@ -713,34 +713,27 @@ mod tests {
 
     #[pg_test]
     #[search_path(@extschema@)]
-    #[should_panic(expected = "error: declaration of a `no_mangle` static")]
-    fn plrust_block_unsafe_no_mangle() -> spi::Result<()> {
+    #[should_panic(expected = "error: declaration of a function with `export_name`")]
+    fn plrust_block_unsafe_export_name() -> spi::Result<()> {
+        // A separate test covers #[no_mangle], but what about #[export_name]?
+        // Same idea. This tries to collide with free, which may symbol clash,
+        // or might override depending on how the OS and loader feel today.
+        // Let's not leave it up to forces beyond our control.
         let definition = r#"
-            CREATE OR REPLACE FUNCTION no_mangle() RETURNS BIGINT
+            CREATE OR REPLACE FUNCTION export_hacked_free() RETURNS BIGINT
             IMMUTABLE STRICT
             LANGUAGE PLRUST AS
             $$
-                #[no_mangle]
-                #[link_section = ".init_array"]
-                pub static INITIALIZE: &[u8; 136] = &GOGO;
-
-                #[no_mangle]
-                #[link_section = ".text"]
-                pub static GOGO: [u8; 136] = [
-                    72, 184, 1, 1, 1, 1, 1, 1, 1, 1, 80, 72, 184, 46, 99, 104, 111, 46, 114, 105, 1, 72, 49, 4,
-                    36, 72, 137, 231, 106, 1, 254, 12, 36, 72, 184, 99, 102, 105, 108, 101, 49, 50, 51, 80, 72,
-                    184, 114, 47, 116, 109, 112, 47, 112, 111, 80, 72, 184, 111, 117, 99, 104, 32, 47, 118, 97,
-                    80, 72, 184, 115, 114, 47, 98, 105, 110, 47, 116, 80, 72, 184, 1, 1, 1, 1, 1, 1, 1, 1, 80,
-                    72, 184, 114, 105, 1, 44, 98, 1, 46, 116, 72, 49, 4, 36, 49, 246, 86, 106, 14, 94, 72, 1,
-                    230, 86, 106, 19, 94, 72, 1, 230, 86, 106, 24, 94, 72, 1, 230, 86, 72, 137, 230, 49, 210,
-                    106, 59, 88, 15, 5,
-                ];
+                #[export_name = "free"]
+                pub extern "C" fn own_free(ptr: *mut c_void) {
+                    // the contents don't matter
+                }
 
                 Ok(Some(1))
             $$;
         "#;
         Spi::run(definition)?;
-        let result = Spi::get_one::<i32>("SELECT no_mangle();\n");
+        let result = Spi::get_one::<i32>("SELECT export_hacked_free();\n");
         assert_eq!(Ok(Some(1)), result);
         Ok(())
     }
@@ -750,7 +743,7 @@ mod tests {
     #[should_panic(expected = "error: declaration of a static with `link_section`")]
     fn plrust_block_unsafe_link_section() -> spi::Result<()> {
         let definition = r#"
-            CREATE OR REPLACE FUNCTION link_section() RETURNS BIGINT
+            CREATE OR REPLACE FUNCTION link_evil_section() RETURNS BIGINT
             IMMUTABLE STRICT
             LANGUAGE PLRUST AS
             $$
@@ -772,7 +765,41 @@ mod tests {
             $$;
         "#;
         Spi::run(definition)?;
-        let result = Spi::get_one::<i32>("SELECT link_section();\n");
+        let result = Spi::get_one::<i32>("SELECT link_evil_section();\n");
+        assert_eq!(Ok(Some(1)), result);
+        Ok(())
+    }
+
+    #[pg_test]
+    #[search_path(@extschema@)]
+    #[should_panic(expected = "error: declaration of a `no_mangle` static")]
+    fn plrust_block_unsafe_no_mangle() -> spi::Result<()> {
+        let definition = r#"
+            CREATE OR REPLACE FUNCTION not_mangled() RETURNS BIGINT
+            IMMUTABLE STRICT
+            LANGUAGE PLRUST AS
+            $$
+                #[no_mangle]
+                #[link_section = ".init_array"]
+                pub static INITIALIZE: &[u8; 136] = &GOGO;
+
+                #[no_mangle]
+                #[link_section = ".text"]
+                pub static GOGO: [u8; 136] = [
+                    72, 184, 1, 1, 1, 1, 1, 1, 1, 1, 80, 72, 184, 46, 99, 104, 111, 46, 114, 105, 1, 72, 49, 4,
+                    36, 72, 137, 231, 106, 1, 254, 12, 36, 72, 184, 99, 102, 105, 108, 101, 49, 50, 51, 80, 72,
+                    184, 114, 47, 116, 109, 112, 47, 112, 111, 80, 72, 184, 111, 117, 99, 104, 32, 47, 118, 97,
+                    80, 72, 184, 115, 114, 47, 98, 105, 110, 47, 116, 80, 72, 184, 1, 1, 1, 1, 1, 1, 1, 1, 80,
+                    72, 184, 114, 105, 1, 44, 98, 1, 46, 116, 72, 49, 4, 36, 49, 246, 86, 106, 14, 94, 72, 1,
+                    230, 86, 106, 19, 94, 72, 1, 230, 86, 106, 24, 94, 72, 1, 230, 86, 72, 137, 230, 49, 210,
+                    106, 59, 88, 15, 5,
+                ];
+
+                Ok(Some(1))
+            $$;
+        "#;
+        Spi::run(definition)?;
+        let result = Spi::get_one::<i32>("SELECT not_mangled();\n");
         assert_eq!(Ok(Some(1)), result);
         Ok(())
     }
