2154bbfd24c0007376dc5fe9538aec133a3c3777
Jubilee <46493976+workingjubilee@users.noreply.github.com>
2023-01-27 12:15:58 -0800

    Ignore Err if remove_dir_all fails (#188)
    * Warn if remove_dir_all fails
    * Warn against discarding user errors
    * Use log instead of warn

--- plrust/src/user_crate/build.rs
+++ plrust/src/user_crate/build.rs
@@ -190,11 +190,10 @@ impl FnBuild {
                         .header("Source Code:")
                 }));
 
-            // Clean up on error but propagate the more relevant error
-            std::fs::remove_dir_all(&self.crate_dir).wrap_err(format!(
-                "Problem deleting temporary crate directory at '{}'",
-                self.crate_dir.display()
-            ));
+            // Clean up on error but don't let this error replace our user's error!
+            if let Err(e)= std::fs::remove_dir_all(&self.crate_dir) {
+                pgx::log!("Problem during removing crate directory: {e}")
+            };
 
             err?
         }
