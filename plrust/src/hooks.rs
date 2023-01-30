dbd1da6f464bb5bd2da8ac7334f752b15a5a39d6
Eric B. Ridge <eebbrr@gmail.com>
2023-01-30 11:36:08 -0500

    trying to lookup the plrust language name while not in a transaction trips a Postgres ASSERT

--- plrust/src/hooks.rs
+++ plrust/src/hooks.rs
@@ -10,6 +10,7 @@ Use of this source code is governed by the PostgreSQL license that can be found 
 use std::ffi::CStr;
 
 use pgx::{pg_guard, pg_sys, PgBox, PgList, PgLogLevel, PgSqlErrorCode};
+use pgx::pg_sys::Oid;
 
 use crate::pgproc::PgProc;
 
@@ -232,8 +233,18 @@ fn lookup_func_lang(pg_proc_oid: pg_sys::Oid) -> eyre::Result<pg_sys::Oid> {
 }
 
 /// Returns [`pg_sys::Oid::INVALID`] if the `plrust` language isn't installed in the current database
-pub(crate) fn plrust_lang_oid() -> pg_sys::Oid {
+fn plrust_lang_oid() -> pg_sys::Oid {
     static PLRUST_LANG_NAME: &[u8] = b"plrust\0"; // want this to look like a c string
+
+    unsafe {
+        // SAFETY:  FFI is always unsafe
+        //
+        // If for some reason we're not currently in a transaction, we can't lookup the plrust
+        // language Oid, so we must return the best value we can: Oid::INVALID
+        if !pg_sys::IsTransactionState() {
+            return Oid::INVALID
+        }
+    }
 
     // SAFETY: We pass `missing_ok: true`, which will return `Oid::INVALID` if the plrust language
     // isn't installed in the current database. The first parameter has the same requirements as `&CStr`.
