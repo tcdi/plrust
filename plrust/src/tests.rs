fd7e3d3bbeaa78722c217c59f015642c925c9523
Thom Chiovoloni <thom@shift.click>
2023-02-17 08:08:29 -0800

    Fix `should_panic` message

--- plrust/src/tests.rs
+++ plrust/src/tests.rs
@@ -452,7 +452,7 @@ mod tests {
     #[cfg(feature = "trusted")]
     #[pg_test]
     #[search_path(@extschema@)]
-    #[should_panic = "error: the `include_str` and `include_bytes` macros are forbidden"]
+    #[should_panic = "error: the `include_str`, `include_bytes`, and `include` macros are forbidden"]
     fn postgrestd_no_include_str() -> spi::Result<()> {
         let definition = r#"
             CREATE FUNCTION include_str()
