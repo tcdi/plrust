9d6a9f8401867fe23fccfee38da16c8c04629806
Thom Chiovoloni <thom@shift.click>
2023-02-22 13:45:22 -0800

    Lint against `dyn Fn{,Mut,Once}` (#226)

--- plrustc/plrustc/src/lints.rs
+++ plrustc/plrustc/src/lints.rs
@@ -100,15 +100,38 @@ declare_lint_pass!(PlrustFnPointer => [PLRUST_FN_POINTERS]);
 
 impl<'tcx> LateLintPass<'tcx> for PlrustFnPointer {
     fn check_ty(&mut self, cx: &LateContext<'tcx>, ty: &hir::Ty) {
-        if let hir::TyKind::BareFn { .. } = &ty.kind {
-            // TODO: ideally this would just be cases where they accept or
-            // return nested references, however doing so is tricky, as it must
-            // pierce through `&'a SomeStruct(&'b InternalRef)`.
-            cx.lint(
-                PLRUST_FN_POINTERS,
-                "Use of function pointers is forbidden in PL/Rust",
-                |b| b.set_span(ty.span),
-            );
+        match &ty.kind {
+            hir::TyKind::BareFn { .. } => {
+                // TODO: ideally this would just be cases where they accept or
+                // return nested references, however doing so is tricky, as it must
+                // pierce through `&'a SomeStruct(&'b InternalRef)`.
+                cx.lint(
+                    PLRUST_FN_POINTERS,
+                    "Use of function pointers is forbidden in PL/Rust",
+                    |b| b.set_span(ty.span),
+                );
+            }
+            hir::TyKind::TraitObject(traits, ..) => {
+                for trayt in *traits {
+                    if let Some(did) = trayt.trait_ref.path.res.opt_def_id() {
+                        let fn_traits = [
+                            &["core", "ops", "function", "Fn"],
+                            &["core", "ops", "function", "FnMut"],
+                            &["core", "ops", "function", "FnOnce"],
+                        ];
+                        for fn_trait_paths in fn_traits {
+                            if match_def_path(cx, did, fn_trait_paths) {
+                                cx.lint(
+                                    PLRUST_FN_POINTERS,
+                                    "Use of function trait objects is forbidden in PL/Rust",
+                                    |b| b.set_span(ty.span),
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+            _ => {}
         }
     }
 }
