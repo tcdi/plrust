457741a68d73dfddce6ece6b3b448bcca477efc4
Eric B. Ridge <eebbrr@gmail.com>
2023-03-09 11:23:07 -0500

    upgrade pgx to 0.7.3

--- upgrade-deps.sh
+++ upgrade-deps.sh
@@ -3,6 +3,9 @@
 # requires:  "cargo install cargo-edit" from https://github.com/killercup/cargo-edit
 
 cargo update
-cargo upgrade
+
+# generally speaking, the only pinned dependency we use is pgx, and generally speaking the only time we run this script
+# is when we want to upgrade to a newer pgx.  `--pinned` has entered the chat
+cargo upgrade --pinned
 cargo generate-lockfile
 
