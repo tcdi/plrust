b2f0bbe881c1fc36397ab1ac7bd7af8750c65c30
Eric B. Ridge <eebbrr@gmail.com>
2023-05-26 16:35:20 -0400

    upgrade dependencies, including pgrx

--- upgrade-deps.sh
+++ upgrade-deps.sh
@@ -6,7 +6,7 @@ cargo update
 
 # generally speaking, the only pinned dependency we use is pgrx, and generally speaking the only time we run this script
 # is when we want to upgrade to a newer pgrx.  `--pinned` has entered the chat
-cargo upgrade --pinned
+cargo upgrade --pinned --incompatible
 cargo generate-lockfile
 
 cd plrustc
