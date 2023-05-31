64ce77b8e540af7cd852a705ec77dfb91341afda
Eric Ridge <eebbrr@gmail.com>
2023-05-31 15:20:05 -0400

    Date/time/interval support (#316)

--- plrust/src/tests.rs
+++ plrust/src/tests.rs
@@ -1123,42 +1123,74 @@ mod tests {
         Ok(())
     }
 
-    // #[pg_test]
-    // fn test_daterange() -> spi::Result<()> {
-    //     Spi::run(
-    //         r#"CREATE FUNCTION test_daterange(r daterange) RETURNS daterange LANGUAGE plrust AS $$ Ok(r) $$"#,
-    //     )?;
-    //     let r = Spi::get_one::<Range<Date>>(
-    //         "SELECT test_daterange('[1977-03-20, 1980-01-01)'::daterange);",
-    //     )?
-    //     .expect("SPI result was null");
-    //     assert_eq!(r, Range::new(Date::new(), Date::new()));
-    //     Ok(())
-    // }
-    //
-    // #[pg_test]
-    // fn test_tsrange() -> spi::Result<()> {
-    //     Spi::run(
-    //         r#"CREATE FUNCTION test_tsrange(p tsrange) RETURNS tsrange LANGUAGE plrust AS $$ Ok(p) $$"#,
-    //     )?;
-    //     let p = Spi::get_one::<pg_sys::Point>("SELECT test_tsrange('42, 99'::tsrange);")?
-    //         .expect("SPI result was null");
-    //     assert_eq!(p.x, 42.0);
-    //     assert_eq!(p.y, 99.0);
-    //     Ok(())
-    // }
-    //
-    // #[pg_test]
-    // fn test_tstzrange() -> spi::Result<()> {
-    //     Spi::run(
-    //         r#"CREATE FUNCTION test_tstzrange(p tstzrange) RETURNS tstzrange LANGUAGE plrust AS $$ Ok(p) $$"#,
-    //     )?;
-    //     let p = Spi::get_one::<pg_sys::Point>("SELECT test_tstzrange('42, 99'::tstzrange);")?
-    //         .expect("SPI result was null");
-    //     assert_eq!(p.x, 42.0);
-    //     assert_eq!(p.y, 99.0);
-    //     Ok(())
-    // }
+    #[pg_test]
+    fn test_daterange() -> Result<(), Box<dyn Error>> {
+        Spi::run(
+            r#"CREATE FUNCTION test_daterange(r daterange) RETURNS daterange LANGUAGE plrust AS $$ Ok(r) $$"#,
+        )?;
+        let r = Spi::get_one::<Range<Date>>(
+            "SELECT test_daterange('[1977-03-20, 1980-01-01)'::daterange);",
+        )?
+        .expect("SPI result was null");
+        assert_eq!(
+            r,
+            Range::new(
+                Date::new(1977, 3, 20)?,
+                RangeBound::Exclusive(Date::new(1980, 01, 01)?)
+            )
+        );
+        Ok(())
+    }
+
+    #[pg_test]
+    fn test_tsrange() -> Result<(), Box<dyn Error>> {
+        Spi::run(
+            r#"CREATE FUNCTION test_tsrange(p tsrange) RETURNS tsrange LANGUAGE plrust AS $$ Ok(p) $$"#,
+        )?;
+        let r = Spi::get_one::<Range<Timestamp>>(
+            "SELECT test_tsrange('[1977-03-20, 1980-01-01)'::tsrange);",
+        )?
+        .expect("SPI result was null");
+        assert_eq!(
+            r,
+            Range::new(
+                Timestamp::new(1977, 3, 20, 0, 0, 0.0)?,
+                RangeBound::Exclusive(Timestamp::new(1980, 01, 01, 0, 0, 0.0)?)
+            )
+        );
+        Ok(())
+    }
+
+    #[pg_test]
+    fn test_tstzrange() -> Result<(), Box<dyn Error>> {
+        Spi::run(
+            r#"CREATE FUNCTION test_tstzrange(p tstzrange) RETURNS tstzrange LANGUAGE plrust AS $$ Ok(p) $$"#,
+        )?;
+        let r = Spi::get_one::<Range<TimestampWithTimeZone>>(
+            "SELECT test_tstzrange('[1977-03-20, 1980-01-01)'::tstzrange);",
+        )?
+        .expect("SPI result was null");
+        assert_eq!(
+            r,
+            Range::new(
+                TimestampWithTimeZone::new(1977, 3, 20, 0, 0, 0.0)?,
+                RangeBound::Exclusive(TimestampWithTimeZone::new(1980, 01, 01, 0, 0, 0.0)?)
+            )
+        );
+        Ok(())
+    }
+
+    #[pg_test]
+    fn test_interval() -> Result<(), Box<dyn Error>> {
+        Spi::run(
+            r#"CREATE FUNCTION get_interval_hours(i interval) RETURNS numeric STRICT LANGUAGE plrust AS $$ Ok(i.extract_part(DateTimeParts::Hour)) $$"#,
+        )?;
+        let hours =
+            Spi::get_one::<AnyNumeric>("SELECT get_interval_hours('3 days 9 hours 12 seconds')")?
+                .expect("SPI result was null");
+        assert_eq!(hours, AnyNumeric::from(9));
+        Ok(())
+    }
 
     #[cfg(feature = "trusted")]
     #[pg_test]
