75aba1fc51539a1de2f1435c00d308fd7659cbd8
Eric B. Ridge <eebbrr@gmail.com>
2023-06-15 14:11:21 -0400

    bootstrap a `plrust-tests` create
    The new `plrust-tests` create is a standalone pgrx crate that only
    contains the unit tests from `plrust`.  These were copied over verbatim.
    
    To run the tests:
    
    ```shell
    $ cd plrust-tests
    $ ./run-tests.sh pgXX [test_name]
    ```

---
+++ plrust-tests/run-tests.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+VERSION=$1
+
+if [ -z ${VERSION} ]; then
+	echo "usage:  ./run-tests.sh pgXX [test-name]"
+	exit 1
+fi
+
+TEST_DIR=`pwd`
+
+# install the plrust extension into the pgrx-managed postgres
+echo "============================"
+echo " installing plrust"
+echo
+cd ../plrust
+echo "\q" | cargo pgrx run ${VERSION}
+
+# run the test suite from this crate
+cd ${TEST_DIR}
+
+echo
+echo "============================"
+echo " running plrust-tests suite"
+echo
+
+cargo pgrx test ${VERSION} $2
+
