6cd04ca1413e6e3983d4730387c1d8398f1ee5a1
Eric B. Ridge <eebbrr@gmail.com>
2023-07-10 15:44:41 -0400

    a script to update the version number

---
+++ update-version.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Portions Copyright 2019-2021 ZomboDB, LLC.
+# Portions Copyright 2021-2022 Technology Concepts & Design, Inc.
+# <support@tcdi.com>
+#
+# All rights reserved.
+#
+# Use of this source code is governed by the MIT license that can be found in
+# the LICENSE file.
+
+##
+## This script requires `cargo install cargo-workspace-version` from https://crates.io/crates/cargo-workspace-version
+##
+
+NEW_VERSION=$1
+
+if [ -z "${NEW_VERSION}" ]; then
+  echo usage:  ./update-version.sh new.version.number
+  exit 1
+fi
+
+## update versions
+cargo workspace-version update v"${NEW_VERSION}"
+cd plrustc && cargo workspace-version update v"${NEW_VERSION}"
